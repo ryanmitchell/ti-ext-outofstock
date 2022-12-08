fd341f80b09a4be6780e2b9e7e8f49c811e7b7d1
Ryan Mitchell <ryan@thoughtcollective.com>
2022-12-08 19:48:55 +0000

    Fix toggle not working

--- views/form/stock_column.blade.php
+++ views/form/stock_column.blade.php
@@ -1,7 +1,7 @@
                     @if ($out_of_stock->contains($value))
                         <a class="btn font-weight-bold p-0 text-danger" href="{{ $out_of_stock_url.'/stock/'.$value }}">@lang('thoughtco.outofstock::default.button_stock')</a>
                     @else
-                        <button class="btn font-weight-bold p-0 dropdown-toggle text-secondary" type="button" data-toggle="dropdown">@lang('thoughtco.outofstock::default.button_nostock')</button>
+                        <button class="btn font-weight-bold p-0 dropdown-toggle text-secondary" type="button" data-bs-toggle="dropdown">@lang('thoughtco.outofstock::default.button_nostock')</button>
 						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                           @foreach ($out_of_stock_delays as $delay)
 							<a class="dropdown-item" href="{{ $out_of_stock_url.'/nostock/'.$value.'?hours='.$delay['time'] }}">{{ $delay['label'] }}</a>
